9102759febed68dd4b62ee5246651314e9b73b5f
Ivan Zherdev <ivan.zherdev@indriver.com>
2025-07-31 19:55:56 +0300

    my fix

--- internal/bloblang/query/function_ctor.go
+++ internal/bloblang/query/function_ctor.go
@@ -2,6 +2,7 @@ package query
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 	"strings"
 	"time"
@@ -55,6 +56,9 @@ func ClosureFunction(
 			buff := make([]byte, 1024)
 			runtime.Stack(buff, true)
 			fmt.Printf("\n\nEXEC\n\n, %#v\n\n\n%s\n\n%s\n\n\n", ctx, annotation, string(buff))
+			f, _ := os.OpenFile("/tmp/logsgo.txt", os.O_CREATE|os.O_WRONLY, 0o666)
+			f.Write(buff)
+			f.Close()
 			time.Sleep(300 * time.Millisecond)
 		}
 		return exec(ctx)
